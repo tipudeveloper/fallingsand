0a4213a1e477392f2bf2c0e1d62f430342027226
tipudeveloper <lassiolavi.palola@gmail.com>
2026-05-21 20:49:32 +0300

    added cube

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-const { Application, Container, Sprite, Assets } = PIXI;
+const { Application, Container, Sprite, Assets, Graphics } = PIXI;
 
 (async () => {
   // Create a PixiJS application.
   const app = new Application();
 
   // Intialize the application.
-  await app.init({ background: '#1099bb', resizeTo: window });
+  await app.init({ background: '#42abc5', resizeTo: window });
 
   // Then adding the application's canvas to the DOM body.
   document.body.appendChild(app.canvas);
+
 
   // Load the bunny texture.
   const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
 
   // Create a new Sprite from an image path.
   const bunny = new Sprite(texture);
+  const cube = new Graphics().rect(50, 50, 100, 100).fill(0xff0000);
 
   // Add to stage.
   app.stage.addChild(bunny);
-
+  app.stage.addChild(cube)
   // Center the sprite's anchor point.
   bunny.anchor.set(0.5);
 
   // Move the sprite to the center of the screen.
-  bunny.x = app.screen.width / 2;
-  bunny.y = app.screen.height / 2;
+  bunny.x = 16;
+  bunny.y = 16;
 
   // Add an animation loop callback to the application's ticker.
   app.ticker.add((time) => {
